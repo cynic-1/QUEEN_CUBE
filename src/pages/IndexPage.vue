5d77b4905af3d0e92ae026d4df7545e4ca47ee51
song <songyx2001@126.com>
2022-03-24 13:40:46 +0800

    INIT PROJECT

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+    <div class="text-h1">h1标题</div>
+    <div class="text-h2">h2标题</div>
+    <div class="text-h3">h3标题</div>
+    <div class="text-h4">h4标题</div>
+    <div class="text-h5">h5标题</div>
+    <div class="text-h6">h6标题</div>
+    <q-card>
+      <q-card-section>
+        <div class="text-h5">h5标题</div>
+        <div class="text-h6">h6标题</div>
+        <q-img
+          alt="Quasar logo"
+          src="~assets/quasar-logo-vertical.svg"
+          style="width: 200px; height: 200px"
+        />
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
