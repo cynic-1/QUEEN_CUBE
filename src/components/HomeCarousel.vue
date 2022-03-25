7d64ff0c4145116e84f33542017d2a89ec20956a
song <songyx2001@126.com>
2022-03-25 12:53:04 +0800

    carousel text

---
+++ src/components/HomeCarousel.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-carousel
+    animated
+    v-model="slide"
+    navigation
+    infinite
+    transition-prev="slide-right"
+    transition-next="slide-left"
+    @mouseenter="autoplay = false"
+    @mouseleave="autoplay = true"
+  >
+    <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg">
+      <div class="gt-xs text-white text-h3 text-weight-bolder carousel-block">QUEENCUBE 1</div>
+      <div class="xs text-white text-h5 text-weight-bolder carousel-block">QUEENCUBE 1</div>
+    </q-carousel-slide>
+    <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" >
+      <div class="gt-xs text-white text-h3 text-weight-bolder carousel-block">QUEENCUBE 1</div>
+      <div class="xs text-white text-h5 text-weight-bolder carousel-block">QUEENCUBE 1</div>
+    </q-carousel-slide>
+    <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
+    <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" />
+  </q-carousel>
+</template>
+
+<script>
+export default {
+  name: "HomeCarousel",
+  data () {
+    return {
+      slide: 1,
+      autoplay: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.carousel-block {
+  position: absolute;
+  bottom: 40%;
+  left: 10%;
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,34 +1,19 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-    <div class="gt-lg text-h1">h1标题</div>
-    <div class="gt-md text-h2">h2标题</div>
-    <div class="gt-sm text-h3">h3标题</div>
-    <div class="gt-xs text-h4">h4标题</div>
-    <div class="lt-sm text-h5">h5标题</div>
-    <div class="text-h6">h6标题</div>
-    <q-card>
-      <q-card-section>
-        <div class="text-h5">h5标题</div>
-        <div class="text-h6">h6标题</div>
-        <q-img
-          alt="Quasar logo"
-          src="~assets/quasar-logo-vertical.svg"
-          style="width: 200px; height: 200px"
-        />
-      </q-card-section>
-    </q-card>
+  <q-page class="flex">
+    <div class="col">
+      <HomeCarousel/>
+    </div>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
+import HomeCarousel from "components/HomeCarousel";
 
 export default defineComponent({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: {
+    HomeCarousel
+  }
 })
 </script>
