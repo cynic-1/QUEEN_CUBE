a955d683f83863eef2762badfefe459396b3e8bd
song <songyx2001@126.com>
2022-03-25 19:12:36 +0800

    search box responsive design

--- src/components/HomeSearch.vue
+++ src/components/HomeSearch.vue
@@ -1,15 +1,25 @@
 <template>
   <div style="background-color: #ADB0AF;" class="col text-center">
-      <div class="q-py-md text-white text-h5">
+      <div class="gt-xs q-py-md text-white text-h5">
         在此搜索您感兴趣的内容
       </div>
-      <div class="q-pb-md" style="width: 30%; display: inline-block;">
+      <div class="gt-xs q-pb-md" style="width: 40%; display: inline-block;">
         <q-input dense square outlined placeholder="键入内容" v-model="text">
           <template v-slot:append>
             <q-icon class="cursor-pointer" name="search" @click="search"/>
           </template>
         </q-input>
       </div>
+    <div class="xs q-py-sm text-white text-h6">
+      在此搜索您感兴趣的内容
+    </div>
+    <div class="xs q-pb-sm" style="width: 60%; display: inline-block;">
+      <q-input dense square outlined placeholder="键入内容" v-model="text">
+        <template v-slot:append>
+          <q-icon class="cursor-pointer" name="search" @click="search"/>
+        </template>
+      </q-input>
+    </div>
 
   </div>
 </template>
