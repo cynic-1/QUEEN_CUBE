ebde8ce6680c75a7baa3ce84949759503402bd53
song <songyx2001@126.com>
2022-03-25 10:55:02 +0800

    add cooperators

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -65,7 +65,7 @@
     </q-page-container>
 
     <q-footer style="color: black">
-        <div class="flex justify-between q-py-lg">
+      <div class="flex justify-between q-py-lg">
           <div class="row q-pl-xl gt-xs">
             <template v-for="x in 2">
               <div class="q-pr-md">
@@ -90,6 +90,31 @@
             Email: <a :href="'mailto:'+email">{{email}}</a>
           </div>
         </div>
+      <q-separator inset color="#979797" spaced/>
+      <div class="flex justify-center">
+         <div class="xs q-pt-xs">QUEENCUBE</div>
+         <div class="gt-xs q-pt-md text-h5">QUEENCUBE</div>
+      </div>
+      <div class="flex justify-center">
+        <div class="xs">
+          <img alt="QUEEN_CUBE_LOGO" src="QUEEN_CUBE_LOGO.png" width="80px">
+        </div>
+        <div class="gt-xs">
+          <img alt="QUEEN_CUBE_LOGO" src="QUEEN_CUBE_LOGO.png" width="144px">
+        </div>
+      </div>
+      <div class="flex justify-center">
+        <div class="xs q-py-md">
+          <template v-for="coop of cooperators">
+            <img :alt="coop.name" :src="coop.logo" width="40px" style="padding-left: 10px;">
+          </template>
+        </div>
+        <div class="gt-xs q-py-xl">
+          <template v-for="coop of cooperators">
+            <img :alt="coop.name" :src="coop.logo" width="100px" style="padding-left: 20px;">
+          </template>
+        </div>
+      </div>
     </q-footer>
   </q-layout>
 </template>
@@ -112,6 +137,36 @@ export default defineComponent({
       isChinese: true,
       address: '上海市徐汇区田林路200号华鑫天地',
       email: 'Info@queencube.cn',
+      cooperators: [
+        {
+          name: '嘉宏集团',
+          logo: 'JHJT_LOGO.png',
+        },
+        {
+          name: '木里木外',
+          logo: 'MULI_LOGO.png',
+        },
+        {
+          name: '新希望集团',
+          logo: 'NEW_HOPE_LOGO.png',
+        },
+        {
+          name: '金地集团',
+          logo: 'GEMDALE_LOGO.png',
+        },
+        {
+          name: '金科集团',
+          logo: 'JINKE_LOGO.png',
+        },
+        {
+          name: '禹州地产',
+          logo: 'YUZHOU_LOGO.png',
+        },
+        {
+          name: '碧桂园',
+          logo: 'BGY_LOGO.png',
+        },
+      ],
       headers: [
         {
           chinese: '产品中心',
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -5,11 +5,11 @@
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
     >
-    <div class="text-h1">h1标题</div>
-    <div class="text-h2">h2标题</div>
-    <div class="text-h3">h3标题</div>
-    <div class="text-h4">h4标题</div>
-    <div class="text-h5">h5标题</div>
+    <div class="gt-lg text-h1">h1标题</div>
+    <div class="gt-md text-h2">h2标题</div>
+    <div class="gt-sm text-h3">h3标题</div>
+    <div class="gt-xs text-h4">h4标题</div>
+    <div class="lt-sm text-h5">h5标题</div>
     <div class="text-h6">h6标题</div>
     <q-card>
       <q-card-section>
