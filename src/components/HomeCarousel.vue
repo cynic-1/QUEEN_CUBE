d1a115ec7932fc7fabde5ee0b29f9d948c648f6c
song <songyx2001@126.com>
2022-03-25 13:00:53 +0800

    carousel responsive design

--- src/components/HomeCarousel.vue
+++ src/components/HomeCarousel.vue
@@ -8,14 +8,47 @@
     transition-next="slide-left"
     @mouseenter="autoplay = false"
     @mouseleave="autoplay = true"
+    class="gt-xs"
   >
     <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg">
-      <div class="gt-xs text-white text-h3 text-weight-bolder carousel-block">QUEENCUBE 1</div>
-      <div class="xs text-white text-h5 text-weight-bolder carousel-block">QUEENCUBE 1</div>
+      <div class="carousel-block">
+        <div class="text-white text-h3 text-weight-bolder ">
+          主标题
+        </div>
+        <div class="q-pt-md text-white text-h5">
+          副标题
+        </div>
+      </div>
+
     </q-carousel-slide>
     <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" >
-      <div class="gt-xs text-white text-h3 text-weight-bolder carousel-block">QUEENCUBE 1</div>
-      <div class="xs text-white text-h5 text-weight-bolder carousel-block">QUEENCUBE 1</div>
+    </q-carousel-slide>
+    <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
+    <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" />
+  </q-carousel>
+  <q-carousel
+    animated
+    v-model="slide"
+    navigation
+    infinite
+    transition-prev="slide-right"
+    transition-next="slide-left"
+    @mouseenter="autoplay = false"
+    @mouseleave="autoplay = true"
+    class="xs"
+    height="340px"
+  >
+    <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg">
+      <div class="carousel-block">
+        <div class="text-white text-h5 text-weight-bolder ">
+          主标题
+        </div>
+        <div class="text-white text-h6">
+          副标题
+        </div>
+      </div>
+    </q-carousel-slide>
+    <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" >
     </q-carousel-slide>
     <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
     <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" />
