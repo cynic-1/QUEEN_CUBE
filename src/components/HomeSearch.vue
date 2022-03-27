a55e94722db32a0e4450fc6786e71cf569446ec3
song <songyx2001@126.com>
2022-03-27 14:22:22 +0800

    packet SearchBox.vue

--- src/components/HomeSearch.vue
+++ src/components/HomeSearch.vue
@@ -1,32 +1,22 @@
 <template>
   <div style="background-color: #ADB0AF;" class="col text-center">
-      <div class="gt-xs q-py-md text-white text-h5">
-        在此搜索您感兴趣的内容
-      </div>
-      <div class="gt-xs q-pb-md" style="width: 40%; display: inline-block;">
-        <q-input dense square outlined placeholder="键入内容" v-model="text">
-          <template v-slot:append>
-            <q-icon class="cursor-pointer" name="search" @click="search"/>
-          </template>
-        </q-input>
-      </div>
+    <div class="gt-xs q-py-md text-white text-h5">
+      在此搜索您感兴趣的内容
+    </div>
     <div class="xs q-py-sm text-white text-h6">
       在此搜索您感兴趣的内容
     </div>
-    <div class="xs q-pb-sm" style="width: 60%; display: inline-block;">
-      <q-input dense square outlined placeholder="键入内容" v-model="text">
-        <template v-slot:append>
-          <q-icon class="cursor-pointer" name="search" @click="search"/>
-        </template>
-      </q-input>
-    </div>
-
+    <SearchBox/>
   </div>
 </template>
 
 <script>
+import SearchBox from "components/SearchBox";
 export default {
   name: "HomeSearch",
+  components: {
+    SearchBox
+  },
   data() {
     return {
       text: '',
---
+++ src/components/SearchBox.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="xs q-pb-sm" style="width: 60%; display: inline-block;">
+    <q-input dense outlined placeholder="键入内容" v-model="text">
+      <template v-slot:append>
+        <q-icon class="cursor-pointer" name="search" @click="search"/>
+      </template>
+    </q-input>
+  </div>
+  <div class="gt-xs q-pb-md" style="width: 30%; display: inline-block;">
+    <q-input dense outlined placeholder="键入内容" v-model="text">
+      <template v-slot:append>
+        <q-icon class="cursor-pointer" name="search" @click="search"/>
+      </template>
+    </q-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBox",
+  methods: {
+    search() {
+      console.log('search!')
+    }
+  },
+  data() {
+    return {
+      text: '',
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
